c1fff1d065cc4d446607493aea9d4171640c21b0
Jonathan Piché <jpiche@coveo.com>
2022-10-12 07:01:33 -0400

    fix test

--- test_coveo_stew/test_poetry_groups.py
+++ test_coveo_stew/test_poetry_groups.py
@@ -5,8 +5,12 @@ _ = poetry_groups_mock
 
 
 def test_poetry_groups(poetry_groups_mock: PythonProject) -> None:
-    assert set(poetry_groups_mock.package.dependencies) == {"python", "requests"}
-    assert set(poetry_groups_mock.package.dev_dependencies) == {"black", "mypy"}
+    assert set(poetry_groups_mock.package.dependencies) == {
+        "python",
+        "requests",
+        "black",
+    }  # black is in extra
+    assert set(poetry_groups_mock.package.dev_dependencies) == {"mypy"}
     assert set(poetry_groups_mock.package.all_dependencies) == {
         "python",
         "requests",
